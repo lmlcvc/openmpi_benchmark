ba323eadc1c861c08f871de0324574dc0768d56d
lmlcvc <lana.milicevic10@gmail.com>
2023-07-04 15:08:35 +0200

    Interval-based communication and TP calculation

--- benchmark.cpp
+++ benchmark.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <iomanip>
 #include <mpi.h>
 #include <chrono>
 
 const std::size_t MESSAGE_SIZE = 1000000;
-const std::size_t NUM_ROUND_TRIPS = 5;
+const std::size_t NUM_ROUND_TRIPS = 10;
+const double INTERVAL_MS = 50.0;
 
 void perform_rt_communication(uint32_t *message, uint8_t rank) {
     MPI_Datatype mpi_uint32_t;
@@ -44,30 +46,50 @@ int main(int argc, char **argv) {
     for (std::size_t i = 0; i < MESSAGE_SIZE; i++) {
         message[i] = i;
     }
+    std::cout << "Message Size: " << static_cast<double>(MESSAGE_SIZE * sizeof(uint32_t) / 1024) << " kB" << std::endl;
 
     // Warmup round
     perform_rt_communication(message, rank);
 
     // Measure elapsed time
     start_time = MPI_Wtime();
-    for (std::size_t i = 0; i < NUM_ROUND_TRIPS; i++) {
-        perform_rt_communication(message, rank);
-        if (rank == 0) {
-            double rtt = MPI_Wtime() - start_time;
-            double rtt_throughput = (double) (MESSAGE_SIZE * sizeof(uint32_t)) / (1024 * 1024 * rtt);
+    double elapsed_ms = 0.0;
+    std::size_t round_trip = 0;
 
-            std::cout << "Round Trip " << i + 1 << " - RTT: " << rtt << " s | Throughput: " << rtt_throughput << " MB/s"
-                      << std::endl;
+    std::size_t total_bytes = 0;
+    double total_rtt = 0.0;
+
+    while (elapsed_ms < NUM_ROUND_TRIPS * INTERVAL_MS) {
+        double current_time = MPI_Wtime();
+        elapsed_time = current_time - start_time;
+        elapsed_ms = elapsed_time * 1000.0;
+
+        if (elapsed_ms >= (round_trip + 1) * INTERVAL_MS) {
+            double rt_start_time = MPI_Wtime();
+            perform_rt_communication(message, rank);
+            double rt_end_time = MPI_Wtime();
+            double rtt = rt_end_time - rt_start_time;
+
+            total_bytes += MESSAGE_SIZE * sizeof(uint32_t);
+            total_rtt += rtt;
+
+            if (rank == 0) {
+                double rtt_throughput = static_cast<double>(total_bytes) / (total_rtt * 1024 * 1024);
+                double avg_throughput = (rtt_throughput * (round_trip + 1)) / (round_trip + 2);
+
+                std::cout << "Round Trip " << round_trip + 1
+                        << " | RTT: " << std::fixed << std::setprecision(6) << rtt
+                        << " ms | Throughput: " << std::fixed << std::setprecision(2) << rtt_throughput
+                        << " MB/s | Average Throughput: " << std::fixed << std::setprecision(2) << avg_throughput
+                        << " MB/s" << std::endl;
+
+                round_trip++;
+            }
         }
     }
+
     end_time = MPI_Wtime();
     elapsed_time = end_time - start_time;
-
-    if (rank == 0) {
-        std::cout << "Elapsed time: " << elapsed_time << " s" << std::endl;
-        double elapsed_throughput = (double) (MESSAGE_SIZE * sizeof(uint32_t)) / (1024 * 1024 * elapsed_time);
-        std::cout << "Elapsed Throughput: " << elapsed_throughput << " MB/s" << std::endl;
-    }
 
     // Free allocated memory and finalize
     delete[] message;
