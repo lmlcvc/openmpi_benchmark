539b1c10f4765e8d1291f1c38ddfe8d561719938
Lana Milicevic <lana.milicevic@cern.ch>
2023-07-25 13:41:00 +0200

    Improve code readability

--- benchmark_oneway.cpp
+++ benchmark_oneway.cpp
@@ -12,6 +12,7 @@
 #include <thread>
 #include <cmath>
 
+// Function to calculate the difference between two timespec structures
 timespec diff(timespec start, timespec end)
 {
     timespec time_diff;
@@ -28,16 +29,42 @@ timespec diff(timespec start, timespec end)
     return time_diff;
 }
 
+
+// Functions for formatted throughput info printing
+void print_continuous(std::size_t round_trip, double rtt, double throughput)
+{
+    std::cout << std::fixed << std::setprecision(8);
+
+    std::cout << "| " << std::left << std::setw(12) << "Round Trip"
+                << " | " << std::right << std::setw(14) << "RTT"
+                << " | " << std::setw(25) << "Throughput"
+                << " |" << std::endl;
+
+    std::cout << "| " << std::left << std::setw(12) << round_trip
+                << " | " << std::right << std::setw(12) << rtt << " s"
+                << " | " << std::setw(18) << throughput << " Mbit/s"
+                << " |\n"
+                << std::endl;
+}
+
+void print_discrete(std::size_t message_size, double throughput)
+{
+    std::cout << std::fixed << std::setprecision(2);
+    std::cout << "| " << std::left << std::setw(12) << message_size
+                        << " | " << std::setw(19) << throughput
+                        << " |\n";
+}
+
+// Function to perform round-trip communication between two processes
 void perform_rt_communication(int8_t *message, std::size_t message_size, std::size_t round_trip,
                               int8_t rank, bool print_enabled = false, bool continuous_send = true)
 {
-
     if (rank == 0)
     {
-        timespec start_time;
+        timespec rt_start_time;
         if (print_enabled)
         {
-            clock_gettime(CLOCK_MONOTONIC, &start_time);
+            clock_gettime(CLOCK_MONOTONIC, &rt_start_time);
         }
 
         MPI_Request send_request;
@@ -49,33 +76,12 @@ void perform_rt_communication(int8_t *message, std::size_t message_size, std::si
         {
             timespec end_time;
             clock_gettime(CLOCK_MONOTONIC, &end_time);
-            timespec elapsed_time = diff(start_time, end_time);
+            timespec elapsed_time = diff(rt_start_time, end_time);
 
             double rtt = elapsed_time.tv_sec + (elapsed_time.tv_nsec / 1e9);
-            double rtt_throughput = (message_size * sizeof(int8_t) * 8) / (rtt * 1000 * 1000);
+            double throughput = (message_size * sizeof(int8_t) * 8) / (rtt * 1000 * 1000);
 
-            std::cout << std::fixed << std::setprecision(2);
-            if (continuous_send)
-            {
-                std::cout << std::fixed << std::setprecision(8);
-
-                std::cout << "| " << std::left << std::setw(12) << "Round Trip"
-                          << " | " << std::right << std::setw(14) << "RTT"
-                          << " | " << std::setw(25) << "Throughput"
-                          << " |" << std::endl;
-
-                std::cout << "| " << std::left << std::setw(12) << round_trip
-                          << " | " << std::right << std::setw(12) << rtt << " s"
-                          << " | " << std::setw(18) << rtt_throughput << " Mbit/s"
-                          << " |\n"
-                          << std::endl;
-            }
-            else
-            {
-                std::cout << "| " << std::left << std::setw(12) << message_size
-                          << " | " << std::setw(19) << rtt_throughput
-                          << " |\n";
-            }
+            continuous_send ? print_continuous(round_trip, rtt, throughput) : print_discrete(message_size, throughput);
         }
     }
     else if (rank == 1)
@@ -83,6 +89,67 @@ void perform_rt_communication(int8_t *message, std::size_t message_size, std::si
         MPI_Request recv_request;
         MPI_Irecv(message, message_size, MPI_BYTE, 0, 0, MPI_COMM_WORLD, &recv_request);
         MPI_Wait(&recv_request, MPI_STATUS_IGNORE);
+    }
+}
+
+// Function to perform continuous round-trip communication
+void setup_continuous_communication(int8_t *message, std::size_t message_size, int8_t rank, std::size_t print_interval)
+{
+    if (rank == 0)
+        std::cout << "Message size: " << message_size << ", interval: " << print_interval << std::endl;
+
+    // time of run start
+    timespec start_time;
+    clock_gettime(CLOCK_MONOTONIC, &start_time);
+
+    std::size_t round_trip = 1;
+    bool running = true;
+
+    while (running)
+    {
+        // handle overflow
+        if (round_trip == 0)
+            clock_gettime(CLOCK_MONOTONIC, &start_time);
+
+        perform_rt_communication(message, message_size, round_trip, rank, (round_trip % print_interval) == 0);
+        round_trip++;
+
+        // TODO: install ncurses on target?
+        /*  ch = getch();
+            if (tolower(ch))
+                running = false; */
+
+    }
+
+    // time of run finish
+    timespec end_time;
+    clock_gettime(CLOCK_MONOTONIC, &end_time);
+    timespec elapsed_time = diff(start_time, end_time);
+    double elapsed_seconds = elapsed_time.tv_sec + (elapsed_time.tv_nsec / 1e9);
+
+    if (rank == 0)
+        std::cout << "Elapsed time: " << elapsed_seconds << " s" << std::endl;
+}
+
+// Function to perform discrete round-trip communication
+void setup_discrete_communication(int8_t *message, int8_t rank, std::size_t max_power = 24)
+{
+    if (rank == 0)
+    {
+        std::cout << std::fixed << std::setprecision(2);
+        std::cout << "| " << std::left << std::setw(12) << "Bytes"
+                    << " | " << std::setw(18) << "Throughput [Mbit/s]"
+                    << " |\n";
+        std::cout << "--------------------------------------\n";
+    }
+
+    std::size_t round_trip = 1;
+
+    for (std::size_t power = 0; power <= max_power; power++)
+    {
+        std::size_t current_message_size = static_cast<std::size_t>(std::pow(2, power));
+        perform_rt_communication(message, current_message_size, round_trip, rank, true, false);
+        round_trip++;
     }
 }
 
@@ -123,71 +190,26 @@ int main(int argc, char **argv)
             continuous_send = false;
             break;
         case 'h':
-        {
-            if (rank != 0)
-                break;
-
-            std::this_thread::sleep_for(std::chrono::milliseconds(100)); // for neater terminal output
-            std::string input;
-
-            // Prompt for continuous run
-            std::cout << "Perform continuous run? (y/N): ";
-            std::getline(std::cin, input);
-
-            if (input.length() > 0 && tolower(input[0]) == 'n')
-            {
-                continuous_send = false;
-                break;
-            }
-
-            const std::size_t max_value = std::numeric_limits<std::size_t>::max();
-            std::cout << "Enter value for message size (current: " << message_size << ", max: " << max_value
-                      << "): ";
-            std::getline(std::cin, input);
-
-            if (input.empty() || (std::stoull(input) > max_value))
-            {
-                std::cout << "\tInput empty or out of bounds. Defaulting to " << message_size << std::endl;
-            }
-            else
-            {
-                message_size = std::stoi(input);
-            }
-
-            std::cout << "Enter value for print interval (current: " << print_interval << "): ";
-            std::getline(std::cin, input);
-
-            if (input.empty() || (std::stoull(input) > max_value))
-            {
-                std::cout << "\tInput empty or out of bounds. Defaulting to " << message_size << std::endl;
-            }
-            else
-            {
-                print_interval = std::stoi(input);
-            }
-
-            break;
-        }
         default:
             if (rank == 0)
                 std::cout << "Usage:" << argv[0] << std::endl
                           << "\t-m <message-size>\n\t-i <print-interval>\n\t-s perform scan\n\t-h <help>" << std::endl;
+            MPI_Finalize();
             return 1;
         }
     }
 
     // Allocate memory with desired alignment
     long page_size = sysconf(_SC_PAGESIZE);
-    std::size_t aligned_size = ((message_size * sizeof(int8_t) - 1) / page_size + 1) * page_size;
-
     void *mem = nullptr;
-    if (posix_memalign(&mem, page_size, aligned_size) != 0)
+    if (posix_memalign(&mem, page_size, message_size) != 0)
     {
         std::cerr << "Memory allocation failed" << std::endl;
+        MPI_Finalize();
         return 1;
     }
 
-    // Initialize message and data
+    // Initialise message and data
     int8_t *message = static_cast<int8_t *>(mem);
     std::fill(message, message + message_size, 0);
 
@@ -196,72 +218,25 @@ int main(int argc, char **argv)
 
     // Scan operation on message sizes
     std::size_t total_rounds;
-    MPI_Allreduce(&message_size, &total_rounds, 1, MPI_UNSIGNED_LONG, MPI_SUM, MPI_COMM_WORLD);
 
-    // Continuous or discrete communication run
-    std::size_t round_trip = 1;
-    timespec start_time;
-    clock_gettime(CLOCK_MONOTONIC, &start_time);
-
-    // Initialize ncurses
+     // Initialize ncurses
     /*     initscr();
         cbreak();              // Disable line buffering
         noecho();              // Disable automatic echoing of characters
         nodelay(stdscr, true); // Make getch() non-blocking
         char ch; */
 
+
     if (continuous_send)
     {
-        if (rank == 0)
-            std::cout << "Message size: " << message_size << ", interval: " << print_interval << std::endl;
-
-        bool running = true;
-        while (running)
-        {
-            // handle overflow
-            if (round_trip == 0)
-                clock_gettime(CLOCK_MONOTONIC, &start_time);
-
-            perform_rt_communication(message, message_size, round_trip, rank, (round_trip % print_interval) == 0);
-            round_trip++;
-
-            // TODO: install ncurses on target?
-            /*         ch = getch();
-                    if (tolower(ch))
-                        running = false; */
-        }
+        setup_continuous_communication(message, message_size, rank, print_interval);        
     }
     else
     {
-        if (rank == 0)
-        {
-            std::cout << std::fixed << std::setprecision(2);
-            std::cout << "| " << std::left << std::setw(12) << "Bytes"
-                      << " | " << std::setw(18) << "Throughput [Mbit/s]"
-                      << " |\n";
-            std::cout << "--------------------------------------\n";
-        }
-
-        std::size_t max_power = 24;
-        for (std::size_t power = 0; power <= max_power; power++)
-        {
-            std::size_t current_message_size = static_cast<std::size_t>(std::pow(2, power));
-            perform_rt_communication(message, current_message_size, round_trip, rank, true, false);
-            round_trip++;
-
-            std::this_thread::sleep_for(std::chrono::milliseconds(75)); // prevent segfault ?
-        }
+        setup_discrete_communication(message, rank);
     }
 
-    timespec end_time;
-    clock_gettime(CLOCK_MONOTONIC, &end_time);
-    timespec elapsed_time = diff(start_time, end_time);
-    double elapsed_seconds = elapsed_time.tv_sec + (elapsed_time.tv_nsec / 1e9);
-
-    if (rank == 0 && continuous_send)
-        std::cout << "Elapsed time: " << elapsed_seconds << " s" << std::endl;
-
-    // Finalize Program
+    // Finalise program
     std::free(mem);
     // endwin();
     MPI_Finalize();
