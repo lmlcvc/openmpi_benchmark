ddf18bcc964edb7c4a3bb9c457d954e180a39cd6
Lana Milicevic <lana.milicevic@cern.ch>
2023-08-17 10:56:38 +0200

    Implement loop receiving

--- communication/communication_interface.cpp
+++ communication/communication_interface.cpp
@@ -7,7 +7,7 @@ std::size_t CommunicationInterface::blockingCommunication(int8_t *bufferSnd, int
 {
     std::vector<MPI_Status> statuses(iterations);
     std::size_t sendOffset = 0, recvOffset = 0;
-    std::size_t remainingSize, wrapSize;
+    std::size_t remainingSize, wrapSize, remainingToReceive, chunkSize;
     std::size_t errorMessageCount = 0;
 
     *transferredSize = messageSize * iterations;
@@ -35,27 +35,30 @@ std::size_t CommunicationInterface::blockingCommunication(int8_t *bufferSnd, int
     {
         for (std::size_t i = 0; i < iterations; i++)
         {
-            MPI_Status status1, status2;
+            MPI_Status status;
+            remainingToReceive = messageSize;
 
-            if (recvOffset + messageSize > rcvBufferBytes)
+            while (remainingToReceive > 0)
             {
-                remainingSize = rcvBufferBytes - recvOffset;
-                wrapSize = messageSize - remainingSize;
+                chunkSize = std::min(remainingToReceive, rcvBufferBytes - recvOffset);
 
-                MPI_Recv(bufferRcv + recvOffset, remainingSize, MPI_BYTE, 0, 0, MPI_COMM_WORLD, &status1);
-                recvOffset = 0;
-                MPI_Recv(bufferRcv + recvOffset, wrapSize, MPI_BYTE, 0, 0, MPI_COMM_WORLD, &status2);
+                MPI_Recv(bufferRcv + recvOffset, chunkSize, MPI_BYTE, 0, 0, MPI_COMM_WORLD, &status);
 
-                if (status1.MPI_ERROR != MPI_SUCCESS)
-                    statuses[i].MPI_ERROR = status1.MPI_ERROR;
-                else if (status2.MPI_ERROR != MPI_SUCCESS)
-                    statuses[i].MPI_ERROR = status2.MPI_ERROR;
+                if (status.MPI_ERROR != MPI_SUCCESS)
+                    statuses[i].MPI_ERROR = status.MPI_ERROR;
+
+                recvOffset = (recvOffset + chunkSize) % rcvBufferBytes;
+                remainingToReceive -= chunkSize;
             }
-            else
-                MPI_Recv(bufferRcv + recvOffset, messageSize, MPI_BYTE, 0, 0, MPI_COMM_WORLD, &statuses[i]);
-
-            recvOffset = (recvOffset + messageSize) % rcvBufferBytes;
         }
+
+        errorMessageCount = std::count_if(statuses.begin(), statuses.end(),
+                                          [](const MPI_Status &status)
+                                          { return status.MPI_ERROR != MPI_SUCCESS; });
+
+        *transferredSize -= messageSize * errorMessageCount;
+
+        return errorMessageCount;
     }
 
     errorMessageCount = std::count_if(statuses.begin(), statuses.end(),
@@ -73,7 +76,8 @@ std::size_t CommunicationInterface::variableBlockingCommunication(int8_t *buffer
 {
     std::vector<MPI_Status> statuses(iterations);
     std::size_t sendOffset = 0, recvOffset = 0;
-    std::size_t messageSize, remainingSize, wrapSize;
+    std::size_t messageSize, remainingSize, wrapSize, remainingToReceive, chunkSize;
+    std::size_t errorMessageCount = 0;
 
     std::random_device rd;
     std::mt19937 generator(rd());
@@ -83,7 +87,7 @@ std::size_t CommunicationInterface::variableBlockingCommunication(int8_t *buffer
     {
         for (std::size_t i = 0; i < iterations; i++)
         {
-            std::size_t messageSize = messageSizes[sizeDistribution(generator)];
+            messageSize = messageSizes[sizeDistribution(generator)];
 
             if (sendOffset + messageSize > sndBufferBytes)
             {
@@ -104,30 +108,28 @@ std::size_t CommunicationInterface::variableBlockingCommunication(int8_t *buffer
     {
         for (std::size_t i = 0; i < iterations; i++)
         {
-            MPI_Status status1, status2;
+            MPI_Status status;
+            remainingToReceive = messageSize;
 
-            if (recvOffset + messageSize > rcvBufferBytes)
+            while (remainingToReceive > 0)
             {
-                remainingSize = rcvBufferBytes - recvOffset;
-                wrapSize = messageSize - remainingSize;
+                chunkSize = std::min(remainingToReceive, rcvBufferBytes - recvOffset);
 
-                MPI_Recv(bufferRcv + recvOffset, remainingSize, MPI_BYTE, 0, 0, MPI_COMM_WORLD, &status1);
-                recvOffset = 0;
-                MPI_Recv(bufferRcv + recvOffset, wrapSize, MPI_BYTE, 0, 0, MPI_COMM_WORLD, &status2);
+                MPI_Recv(bufferRcv + recvOffset, chunkSize, MPI_BYTE, 0, 0, MPI_COMM_WORLD, &status);
 
-                if (status1.MPI_ERROR != MPI_SUCCESS)
-                    statuses[i].MPI_ERROR = status1.MPI_ERROR;
-                else if (status2.MPI_ERROR != MPI_SUCCESS)
-                    statuses[i].MPI_ERROR = status2.MPI_ERROR;
+                if (status.MPI_ERROR != MPI_SUCCESS)
+                    statuses[i].MPI_ERROR = status.MPI_ERROR;
+
+                recvOffset = (recvOffset + chunkSize) % rcvBufferBytes;
+                remainingToReceive -= chunkSize;
             }
-            else
-                MPI_Recv(bufferRcv + recvOffset, messageSize, MPI_BYTE, 0, 0, MPI_COMM_WORLD, &statuses[i]);
 
-            recvOffset = (recvOffset + messageSize) % rcvBufferBytes;
-            if (statuses[i].MPI_ERROR == MPI_SUCCESS)
-                *transferredSize += messageSize;
+            if (statuses[i].MPI_ERROR != MPI_SUCCESS)
+                errorMessageCount++;
         }
     }
+
+    *transferredSize -= messageSize * errorMessageCount;
 
     return std::count_if(statuses.begin(), statuses.end(),
                          [](const MPI_Status &status)
