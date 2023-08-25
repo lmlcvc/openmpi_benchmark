d3988a44cdc9b8f398668cd052dab1f9c8473130
Lana Milicevic <lana.milicevic@cern.ch>
2023-08-25 16:02:19 +0200

    Implement other unit-based communication modes

--- communication/communication_interface.cpp
+++ communication/communication_interface.cpp
@@ -147,6 +147,63 @@ std::pair<std::size_t, std::size_t> CommunicationInterface::nonBlockingCommunica
     return std::make_pair(errorMessageCount, transferredSize);
 }
 
+std::pair<std::size_t, std::size_t> CommunicationInterface::unitsNonBlockingCommunication(ReadoutUnit *ru, BuilderUnit *bu, int ruRank, int buRank, int processRank,
+                                                                                          std::size_t messageSize, std::size_t iterations, std::size_t syncIterations)
+{
+    std::vector<MPI_Request> sendRequests(iterations);
+    std::vector<MPI_Request> recvRequests(iterations);
+    std::vector<MPI_Status> statuses(iterations);
+
+    std::size_t errorMessageCount = 0;
+    std::size_t transferredSize = messageSize * iterations;
+
+    if (processRank == ruRank)
+    {
+        int8_t *bufferSnd = ru->getBuffer();
+        const std::size_t sndBufferBytes = ru->getBufferBytes();
+        std::size_t sendOffset = 0;
+
+        for (std::size_t i = 0; i < iterations; i++)
+        {
+            if (sendOffset + messageSize > sndBufferBytes)
+                sendOffset = 0;
+
+            MPI_Isend(bufferSnd + sendOffset, messageSize, MPI_BYTE, buRank, 0, MPI_COMM_WORLD, &sendRequests[i]);
+
+            sendOffset = (sendOffset + messageSize) % sndBufferBytes;
+
+            if (iterations % syncIterations == 0)
+                MPI_Waitall(i + 1, sendRequests.data(), statuses.data());
+        }
+        MPI_Waitall(iterations, sendRequests.data(), statuses.data());
+    }
+    else if (processRank == buRank)
+    {
+        int8_t *bufferRcv = bu->getBuffer();
+        const std::size_t rcvBufferBytes = bu->getBufferBytes();
+        std::size_t recvOffset = 0;
+
+        for (std::size_t i = 0; i < iterations; i++)
+        {
+            if (recvOffset + messageSize > rcvBufferBytes)
+                recvOffset = 0;
+
+            MPI_Irecv(bufferRcv + recvOffset, messageSize, MPI_BYTE, ruRank, 0, MPI_COMM_WORLD, &recvRequests[i]);
+
+            recvOffset = (recvOffset + messageSize) % rcvBufferBytes;
+        }
+        MPI_Waitall(iterations, recvRequests.data(), statuses.data());
+    }
+
+    errorMessageCount = std::count_if(statuses.begin(), statuses.end(),
+                                      [](const MPI_Status &status)
+                                      { return status.MPI_ERROR != MPI_SUCCESS; });
+
+    transferredSize -= messageSize * errorMessageCount;
+
+    return std::make_pair(errorMessageCount, transferredSize);
+}
+
 std::pair<std::size_t, std::size_t> CommunicationInterface::variableBlockingCommunication(int8_t *bufferSnd, int8_t *bufferRcv,
                                                                                           std::size_t sndBufferBytes, std::size_t rcvBufferBytes,
                                                                                           std::vector<std::size_t> messageSizes, int rank, std::size_t iterations)
@@ -191,6 +248,67 @@ std::pair<std::size_t, std::size_t> CommunicationInterface::variableBlockingComm
             MPI_Recv(bufferRcv + recvOffset, rcvMessageSize, MPI_BYTE, 0, 0, MPI_COMM_WORLD, &statuses[i]);
 
             transferredSize += rcvMessageSize; // Increment transferredSize
+            recvOffset = (recvOffset + rcvMessageSize) % rcvBufferBytes;
+        }
+    }
+
+    std::size_t errorMessageCount = std::count_if(statuses.begin(), statuses.end(),
+                                                  [](const MPI_Status &status)
+                                                  { return status.MPI_ERROR != MPI_SUCCESS; });
+
+    return std::make_pair(errorMessageCount, transferredSize);
+}
+
+std::pair<std::size_t, std::size_t> CommunicationInterface::unitsVariableBlockingCommunication(ReadoutUnit *ru, BuilderUnit *bu, int ruRank, int buRank, int processRank,
+                                                                                              std::vector<std::size_t> messageSizes, std::size_t iterations)
+{
+    std::vector<MPI_Status> statuses(iterations);
+
+    std::size_t transferredSize = 0;
+
+    if (processRank == ruRank)
+    {
+        int8_t *bufferSnd = ru->getBuffer();
+        const std::size_t sndBufferBytes = ru->getBufferBytes();
+        std::size_t sendOffset = 0;
+
+        int sndMessageSize;
+        std::random_device rd;
+        std::mt19937 generator(rd());
+        std::uniform_int_distribution<std::size_t> sizeDistribution(0, messageSizes.size() - 1);
+
+        for (std::size_t i = 0; i < iterations; i++)
+        {
+            sndMessageSize = static_cast<int>(messageSizes[sizeDistribution(generator)]);
+
+            MPI_Send(&sndMessageSize, 1, MPI_INT, buRank, 0, MPI_COMM_WORLD); // Communicate message size over network
+
+            if (sendOffset + sndMessageSize > sndBufferBytes)
+                sendOffset = 0;
+
+            MPI_Send(bufferSnd + sendOffset, sndMessageSize, MPI_BYTE, buRank, 0, MPI_COMM_WORLD);
+
+            transferredSize += sndMessageSize; 
+            sendOffset = (sendOffset + sndMessageSize) % sndBufferBytes;
+        }
+    }
+    else if (processRank == buRank)
+    {
+        int8_t *bufferRcv = bu->getBuffer();
+        const std::size_t rcvBufferBytes = bu->getBufferBytes();
+        std::size_t recvOffset = 0;
+        std::size_t rcvMessageSize;
+
+        for (std::size_t i = 0; i < iterations; i++)
+        {
+            MPI_Recv(&rcvMessageSize, 1, MPI_INT, ruRank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE); // Receive the messageSize from rank 0
+
+            if (recvOffset + rcvMessageSize > rcvBufferBytes)
+                recvOffset = 0;
+
+            MPI_Recv(bufferRcv + recvOffset, rcvMessageSize, MPI_BYTE, ruRank, 0, MPI_COMM_WORLD, &statuses[i]);
+
+            transferredSize += rcvMessageSize;
             recvOffset = (recvOffset + rcvMessageSize) % rcvBufferBytes;
         }
     }
